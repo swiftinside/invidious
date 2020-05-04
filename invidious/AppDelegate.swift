8470e46bbed2bb36699e9cea250f6615747258fe
Francesco Pierfederici <me@pythoninside.com>
2020-05-04 16:47:58 +0200

    restored

--- invidious/AppDelegate.swift
+++ invidious/AppDelegate.swift
@@ -10,15 +10,14 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        // pass
     }
-    
+
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-    
+
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
--- invidious/ViewController.swift
+++ invidious/ViewController.swift
@@ -9,20 +9,174 @@
 import Cocoa
 import SafariServices.SFSafariApplication
 
+// Constants
+let kSelectedUrlKey = "InstanceURL"
+let kSelectedNameKey = "SelectedInstanceName"
+
 class ViewController: NSViewController {
 
     @IBOutlet var appNameLabel: NSTextField!
-    
+    @IBOutlet weak var instancePopUpButton: NSPopUpButton!
+    var instanceDict = [String: String]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.appNameLabel.stringValue = "invidious";
+        self.appNameLabel.stringValue = "invidious"
+
+        // Preferences / Defaults handling
+        //
+        // We use two types of preferences and two types of defaults:
+        // 1. This app preferences (and corresponding defaults) to populate the popup menu.
+        // 2. Shared preferences to communicate the selected instance URL to the extension.
+        //
+        // The only actor writing to both preferences is this app. The extension only reads.
+        //
+        // The flow is as follows:
+        // 1. Iff the app preferences do not exist then read defaults and use them to create
+        //    the preferences plist
+        // 2. Read the preferences plsit into a dictionary
+        // 3. Populate the pop-up menu with the keys from the prefereces dictionary
+        // 4. Do the same for the shared preferences.
+        // 5. Select the item in the pop-up menu according to the "selectedInstance"
+        //    key in the shared oreferences.
+        //
+        // a. Every time the user selects an intem in the pop-up menu, update the
+        //    "selectedInstance" entry into the shared preferences.
+
+        // Factory defaults
+        self.registerDefaults()
+
+        // Recover info on the selected instance from the global/local preferences.
+        guard let dict = self.getInstanceDict() else {
+            return displayPreferencesErrorAndQuit(.local)
+        }
+        self.instanceDict = dict
+
+        guard let (selectedInstanceName, selectedInstanceUrl) = self.getSelectedInstanceInfo() else {
+            // Ops! This should never happen: things are quite messed up?
+            return displayPreferencesErrorAndQuit(.local)
+        }
+
+        self.populateInstancePopUpButton(instanceNames: Array(self.instanceDict.keys),
+                                         selectedName: selectedInstanceName)
+
+        // Shared Preferences
+        guard let sharedUserDefaults = UserDefaults(suiteName: "com.pythoninside.invidiousGroup") else {
+            return displayPreferencesErrorAndQuit(.shared)
+        }
+
+        sharedUserDefaults.set(selectedInstanceUrl as String, forKey: kSelectedUrlKey)
+        print("Shared Preferences: (over)wrote \(kSelectedNameKey) = \(selectedInstanceUrl)")
     }
-    
+
+    func getInstanceDict() -> [String: String]? {
+        // TODO: refactor as function, not method?
+
+        let localUserDefaults = UserDefaults.standard
+
+        let instanceDictKey = "Instances"
+        if let instanceDict = localUserDefaults.dictionary(forKey: instanceDictKey) as? [String: String] {
+            // Cool, things look fine for now. Now grab the name of the selected instance.
+            // Make sure that the instanceDict is not empty! Otherwise we have a big problem.
+            if instanceDict.capacity < 1 {
+                displayPreferencesErrorAndQuit(.local)
+            }
+            return instanceDict
+        }
+        return nil
+    }
+
+    func getSelectedInstanceInfo() -> (String, String)? {
+        let localUserDefaults = UserDefaults.standard
+
+        let selectedNameKey = "SelectedInstanceName"
+        var selectedInstanceUrl = ""
+        var selectedInstanceName = ""
+
+        if let name = localUserDefaults.string(forKey: selectedNameKey),
+            let url = self.instanceDict[name] {
+            selectedInstanceUrl = url
+            selectedInstanceName = name
+            print("Found selected instance name in local/global preferences")
+        } else {
+            // Weird: either we could not find the name of the selected instance
+            // or we cound not find the corresponding URL in instanceDict. We will
+            // revert to the name/URL of the first instance in instanceDict.
+            print("Cannot find selected instance name in local/global preferences")
+
+            if let nameUrl = instanceDict.first {
+                selectedInstanceUrl = nameUrl.value
+                selectedInstanceName = nameUrl.key
+
+                // Fix local preferences.
+                localUserDefaults.set(selectedInstanceName, forKey: selectedNameKey)
+            } else {
+                return nil
+            }
+        }
+        // Here the local preference plist is already created.
+
+        print("Selected instance Name: \(selectedInstanceName)")
+        print("Selected instance URL: \(selectedInstanceUrl)")
+        return (selectedInstanceName, selectedInstanceUrl)
+    }
+
+    func registerDefaults() {
+        // Some sensible defaults
+        var defaults: [String: Any] = [:]
+
+        if let defaultsPath = Bundle.main.path(forResource: "Defaults", ofType: "plist") {
+            if let dict = NSDictionary(contentsOfFile: defaultsPath) as? [String: Any] {
+                defaults.merge(dict) { (_, new) in new }
+            }
+        }
+
+        // You can use this opportunity to insert any default values that need
+        //  to be computed at runtime and thus can't be stored in Defaults.plist.
+        let computedDefaults: [String: Any] = [:]
+        defaults.merge(computedDefaults) { (_, new) in new }
+
+        UserDefaults.standard.register(defaults: defaults)
+    }
+
+    func populateInstancePopUpButton(instanceNames: [String], selectedName: String) {
+        self.instancePopUpButton.removeAllItems()
+        var selectedIndex = 0
+        for (index, name) in sorted(instanceNames).enumerated() {
+            self.instancePopUpButton.addItem(withTitle: name)
+            if name == selectedName {
+                selectedIndex = index
+            }
+        }
+        self.instancePopUpButton.selectItem(at: selectedIndex)
+    }
+
+    @IBAction func selectInstance(_ sender: NSPopUpButton) {
+        if let selectedMenuItem = sender.item(at: sender.indexOfSelectedItem) {
+            print("User selected instance \(selectedMenuItem)")
+
+            // Update both the shared and local preferences.
+            let selectedInstanceName = selectedMenuItem.title
+
+            if let selectedInstanceUrl = self.instanceDict[selectedInstanceName] {
+                let localUserDefaults = UserDefaults.standard
+
+                guard let sharedUserDefaults = UserDefaults(suiteName: "com.pythoninside.invidiousGroup") else {
+                    return
+                }
+
+                localUserDefaults.set(selectedInstanceName, forKey: kSelectedNameKey)
+                sharedUserDefaults.set(selectedInstanceUrl as String, forKey: kSelectedUrlKey)
+            }
+            // else: we got an error: let's ignore that and hope for the best :-)
+        }
+    }
+
     @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
-        SFSafariApplication.showPreferencesForExtension(withIdentifier: "com.pythoninside.invidious-Extension") { error in
-            if let _ = error {
-                // Insert code to inform the user that something went wrong.
-                print("Something went wrong")
+        SFSafariApplication.showPreferencesForExtension(
+        withIdentifier: "com.pythoninside.invidious-Extension") { error in
+            if error != nil {
+                displayGenericErrorAndQuit()
             }
         }
     }
